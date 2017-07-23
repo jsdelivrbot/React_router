e096f23982a882a625c2e4a664051954be112d54
lupan.daniel <lupan.daniel@gmail.com>
2017-07-23 18:48:09 +0300

    initial commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,5 @@
 import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
+export const reducers = combineReducers({
 
-export default rootReducer;
+});
