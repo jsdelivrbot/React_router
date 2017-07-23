59a4a82675af8e4d2b7267a6e6cafbf57236f840
lupan.daniel <lupan.daniel@gmail.com>
2017-07-23 20:21:38 +0300

    import redux-form reducer and assign it to formReducer variable. Wire form reducer to application state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer-posts';
+import { reducer as reducerForm } from 'redux-form';
 
 export const reducers = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: reducerForm
 });
