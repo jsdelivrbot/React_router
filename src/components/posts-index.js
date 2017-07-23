7b601ef80fa5a40e568f9e5fe744c9018bbf92ef
lupan.daniel <lupan.daniel@gmail.com>
2017-07-23 20:03:58 +0300

    Rendering list of posts. ComponentDidMount calls fetchPosts . Lodash creates array from obj

--- src/components/posts-index.js
+++ src/components/posts-index.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import { fetchPosts } from "../actions/index";
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 class PostsIndex extends Component {
 
-  componentDidMount() {
-    this.props.fetchPosts();
-  }
+  componentDidMount() {                     // When component renders, the list of posts is empty
+    this.props.fetchPosts();                // componentDidMount is called by react after the component is rendered one
+                                            // time in the DOM
+  }                                         // Exact name
 
   render() {
-    console.log(this.props.posts);
+                                            // _.map creates an array from object, used for the component to render it
+    const posts = _.map(this.props.posts, (post) => {
+      return (
+        <li
+          key={ post.id }
+          className="list-group-item">
+          { post.title }
+        </li>
+      );
+    });
+
     return (
-      <div>Posts index</div>
+      <div>
+        <h3> Your list of posts</h3>
+        <ul className="list-group">
+          { posts }
+        </ul>
+      </div>
     );
   }
 }
