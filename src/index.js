2d68dd37ca2eb2dda6e5c6050b73ef205d448de1
lupan.daniel <lupan.daniel@gmail.com>
2017-07-23 20:11:29 +0300

    Wired posts-new component. Switch deals with specific routes access

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { reducers } from "./reducers";
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PostsIndex from './components/posts-index';
+import PostsNew from './components/posts-new';
 import promise from 'redux-promise';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -13,7 +14,10 @@ ReactDOM.render(
   <Provider store={ createStoreWithMiddleware(reducers) }>
     <BrowserRouter>
       <div>
-        <Route path="/" component={ PostsIndex }/>
+        <Switch>
+          <Route path="/posts/new" component={ PostsNew }/>
+          <Route path="/" component={ PostsIndex }/>
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
