164bda9d5a8732b0acd5fee2b1a4c2d19f5d28bc
lupan.daniel <lupan.daniel@gmail.com>
2017-07-23 20:36:45 +0300

    Wired the input to the Field. Added Field event handlers to input

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 class PostsNew extends Component {
+
+// The field argument is an object which contains some event handlers that need to be wired to the JSX
+// The Field component is responsible for any changes on this input
+// field.input is an object which contains event handlers i.e. onChange, onBlur, onFocus
+
+  renderField(field) {
+    return (
+      <div>
+        <input
+          type="text"
+          { ...field.input }
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
         <form>
           <Field
             name="title"
-            component={ }
+            component={ this.renderField }
           />
         </form>
       </div>
