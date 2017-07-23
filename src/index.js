6d94f6759abb8248ad69e7c6bc745dd713a0d636
lupan.daniel <lupan.daniel@gmail.com>
2017-07-23 19:13:04 +0300

    added posts-index component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
 import { reducers } from "./reducers";
+import PostsIndex from './components/posts-index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={ createStoreWithMiddleware(reducers) }>
-    <App/>
+
   </Provider>
   , document.querySelector('.container'));
