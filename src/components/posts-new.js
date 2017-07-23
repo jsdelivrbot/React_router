c5856e2572a62b5bf8f570bf59af28b923c7abed
lupan.daniel <lupan.daniel@gmail.com>
2017-07-23 20:32:12 +0300

    Added redux-form, Field. Created new form and wired it to component

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,11 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
-    return(
-      <div>New post</div>
+    return (
+      <div>
+        <form>
+          <Field
+            name="title"
+            component={ }
+          />
+        </form>
+      </div>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'                            // The form name has to be unique
+})(PostsNew);
